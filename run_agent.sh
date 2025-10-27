02f083dfe8f0a550a60e99631ccf515cb60f951b
Ashvin Gupta <qc25022@ddy93.apocrita>
2025-10-27 15:11:36 +0000

    Changing script

--- run_agent.sh
+++ run_agent.sh
@@ -8,11 +8,14 @@
 #$ -e /data/home/qc25022/CancEHR-Training/HPC_Files/loge/
 #$ -j n
 
+cd /data/home/qc25022/CancEHR-Training/
+export PYTHONPATH="/data/home/qc25022/CancEHR-Training/:$PYTHONPATH"
+
 # 1. Activate your environment
 source /data/home/qc25022/CancEHR-Training/venv/bin/activate
 
 # 2. Define your Sweep ID (get this from the 'wandb sweep' command)
-SWEEP_ID="ashvingupta00/ehr-llm-pretraining/9eyqcotp"
+SWEEP_ID="ashvingupta00-imperial-college-london/CancEHR-Training-src_experiments/9e65pi1t"
 
 # 4. Run the agent
 # It will execute this command over and over with new sweep params
