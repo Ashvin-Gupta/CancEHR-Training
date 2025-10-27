1d427940a0b398eebbf20c3a3483dbbc28895c7b
Ashvin-Gupta <ashvingupta00@gmail.com>
2025-10-27 11:55:30 +0000

    chaning config to include lora hyperparameters

--- run_experiment.sh
+++ run_experiment.sh
@@ -29,8 +29,8 @@ echo "Starting experiment from directory: $(pwd)"
 #    --config_name cprd_decoder_lstm_test \
 #    --experiment_name exp_001 
 
-# python -m src.experiments.run_llm_pretrain --config_filepath src/experiments/configs/llm_pretrain.yaml
-python -m src.experiments.create_embedding_corpus --config_filepath src/experiments/configs/embed_text.yaml
+python -m src.experiments.run_llm_pretrain --config_filepath src/experiments/configs/llm_pretrain.yaml
+# python -m src.experiments.create_embedding_corpus --config_filepath src/experiments/configs/embed_text.yaml
 
 # python -m src.experiments.run_llm --config_filepath src/experiments/configs/llm_pretrain.yaml
 echo "Pipeline finished."
