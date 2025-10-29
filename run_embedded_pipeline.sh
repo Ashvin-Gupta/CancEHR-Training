37acb9dfb43ad875675669c533d2725d504c1f2c
Ashvin-Gupta <ashvingupta00@gmail.com>
2025-10-29 17:38:26 +0000

    Adding embedding version

---
+++ run_embedded_pipeline.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#$ -cwd                 
+#$ -pe smp 4
+#$ -l h_rt=1:0:0
+#$ -l h_vmem=12G
+#$ -j n
+#$ -o /data/home/qc25022/CancEHR-Training/HPC_New/logo/
+#$ -e /data/home/qc25022/CancEHR-Training/HPC_New/loge/
+
+set -e 
+
+# Set the base directory for your project
+BASE_DIR="/data/home/qc25022/CancEHR-Training"
+
+export WANDB_API_KEY="3256683a0a9a004cf52e04107a3071099a53038e"
+
+# --- Environment Setup ---
+module load intel intel-mpi python
+source /data/home/qc25022/CancEHR-Training/venv/bin/activate
+
+# --- Execute from Project Root ---
+# Change to the base directory before running the python command
+cd "${BASE_DIR}"
+
+echo "Starting run embedded pipeline experiment from directory: $(pwd)"
+
+# python -m src.experiments.run_llm_pretrain --config_filepath src/experiments/configs/llm_pretrain.yaml
+python -m src.pipelines.embedded_based.create_vocab_embeddings --config_filepath src/pipelines/embedded_based/configs/create_embeddings.yaml
+python -m src.pipelines.embedded_based.create_embeddings --config_filepath src/pipelines/embedded_based/configs/create_embeddings.yaml
+python -m src.pipelines.embedded_based.pretrain --config_filepath src/pipelines/embedded_based/configs/pretrain_encoder_embedded.yaml
+
+echo "Pipeline finished."
+deactivate
+
