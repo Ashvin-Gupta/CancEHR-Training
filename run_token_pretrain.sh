5d72cd8250b74c7cc65921a2ffde84a1c5c1f743
Ashvin-Gupta <ashvingupta00@gmail.com>
2025-10-29 11:48:54 +0000

    Adding token funciton

---
+++ run_token_pretrain.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#$ -cwd                 
+#$ -pe smp 4
+#$ -l h_rt=1:0:0
+#$ -l h_vmem=4G
+#$ -j n
+#$ -o /data/home/qc25022/CancEHR-Training/HPC_New/logo/
+#$ -e /data/home/qc25022/CancEHR-Training/HPC_New/loge/
+
+set -e 
+
+# Set the base directory for your project
+BASE_DIR="/data/home/qc25022/CancEHR-Training"
+
+export WANDB_API_KEY="3256683a0a9a004cf52e04107a3071099a53038e"
+
+# --- Environment Setup ---
+module load intel intel-mpi python
+source /data/home/qc25022/CancEHR-Training/venv/bin/activate
+
+# --- Execute from Project Root ---
+# Change to the base directory before running the python command
+cd "${BASE_DIR}"
+
+echo "Starting experiment from directory: $(pwd)"
+
+python -m src.pipelines.token_based.pretrain --config_filepath src/pipelines/token_based/configs/encoder_lstm.yaml --experiment_name my_lstm_exp
+
+echo "Pipeline finished."
+deactivate
+
