f3c5aa6ee97d571d2f8a6d488faefe1742dcafcd
Ashvin-Gupta <ashvingupta00@gmail.com>
2025-11-03 15:58:07 +0000

    Adding function to translate tokens

--- src/pipelines/text_based/pretrain_llm.py
+++ src/pipelines/text_based/pretrain_llm.py
@@ -144,12 +144,10 @@ def main(config_path: str):
     
     translator = EHRTokenTranslator(data_config["medical_lookup_filepath"], data_config["lab_lookup_filepath"])
     unique_concepts = translator.extract_translated_concepts(data_config["vocab_filepath"])
-    base_model_name = model_config['model_name'].replace('unsloth/', '').replace('-unsloth-bnb-4bit', '')
-    print(f"Base model name: {base_model_name}")
     
     # Perform token adaptation BEFORE applying LoRA
     model, tokenizer = translator.token_adaptation(
-        original_model_name=base_model_name,
+        original_model_name=model_config['base_model_name'],
         unsloth_model_name=model_config['model_name'],
         new_concepts=unique_concepts,
         max_seq_length=model_config['max_length'],  # Pass the max_length from config
