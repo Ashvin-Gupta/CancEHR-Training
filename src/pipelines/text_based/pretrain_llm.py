45a48c6accd94e61dfefc367d4016b4d942021a4
Ashvin-Gupta <ashvingupta00@gmail.com>
2025-11-10 16:52:02 +0000

    Adding warm up where only train on embeddings

--- src/pipelines/text_based/pretrain_llm.py
+++ src/pipelines/text_based/pretrain_llm.py
@@ -390,8 +390,8 @@ def main(config_path: str):
     
     warmup_args = TrainingArguments(
         output_dir=training_config['output_dir'],
-        # num_train_epochs=1,
-        num_train_steps=200,
+        num_train_epochs=0.3,
+        # num_train_steps=200,
         learning_rate=5e-4,
         per_device_train_batch_size=training_config['batch_size'],
         logging_steps=50,
