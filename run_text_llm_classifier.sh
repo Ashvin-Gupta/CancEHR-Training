c0fea5936c6c65108fcc529635f7448984821f3c
Ashvin Gupta <qc25022@login-03.apocrita>
2025-11-18 14:20:31 +0000

    Changing script

--- run_text_llm_classifier.sh
+++ run_text_llm_classifier.sh
@@ -13,9 +13,10 @@ export CUDA_VISIBLE_DEVICES=0
 CONFIG_PATH="src/pipelines/text_based/configs/llm_finetune_classifier.yaml"
 
 # Run the fine-tuning script
-python -m src.pipelines.text_based.finetune_llm_classifier \
+#python -m src.pipelines.text_based.finetune_llm_classifier \
+#    --config_filepath "$CONFIG_PATH"
+python -m src.pipelines.text_based.test_classifier_setup.py \
     --config_filepath "$CONFIG_PATH"
-
 echo "Classification fine-tuning complete!"
 
 
